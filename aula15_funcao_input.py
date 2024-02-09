13a2a10578bf686dfb761d5cfb7faeedd6fa2dda
DionesFernandes <dionesfernandes7@yahoo.com.br>
2024-02-09 11:34:20 -0300

    Primeiro commit

---
+++ aula15_funcao_input.py
@@ -0,0 +1,7 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome}')
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+print(f'Resultado da coleta dos dados é: {numero_1, numero_2}')
