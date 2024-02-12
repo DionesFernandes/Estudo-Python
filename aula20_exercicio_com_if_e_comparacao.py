d6aa55702f933bc0723da1b33e90259ad64173eb
DionesFernandes <dionesfernandes7@yahoo.com.br>
2024-02-11 23:51:37 -0300

    Exercicio com if e comparaçaõ

---
+++ aula20_exercicio_com_if_e_comparacao.py
@@ -0,0 +1,11 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print('primeiro valor é maior que segundo valor')
+
+elif primeiro_valor == segundo_valor:
+    print('primeiro valor é igual a segundo valor')
+    
+else:
+    print('primeiro valor é menor que segundo valor')
