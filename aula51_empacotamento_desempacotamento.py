7c038352621c02a4a7621a7d574b35738bda54d7
DionesFernandes <dionesfernandes7@yahoo.com.br>
2024-02-23 12:18:23 -0300

    Introdução ao empacotamento e desempacotamento

---
+++ aula51_empacotamento_desempacotamento.py
@@ -0,0 +1,9 @@
+"""
+Introdução ao desempacotamento
+"""
+
+_, _, nome, *resto = ['Maria', 'Helena', 'Luiz'] 
+print(nome)
+
+# os sinais _ estão sendo usados para guardar os valores que não estão sendo usados
+# *resto esta sendo usado para guardar o resto dos valores da lista
